08d0b7630aaa88c4886165ee2ae798398135e71d
seeatulanand <atulanandbhatt@gmail.com>
2024-07-11 08:49:42 +0530

    Break Singleton design using refelection Api

--- src/singletondesign/Employee.java
+++ src/singletondesign/Employee.java
@@ -3,11 +3,15 @@ package singletondesign;
 public class Employee {
 
     private static volatile Employee INSTANCE=null;
-
     private Employee(){
+        // to stop breaking the singleton design pattern in constructor check object is created
+        //or not. If created throw exception.
+
+        if(INSTANCE!=null){
+            throw new RuntimeException("Trying to break singleton pattern");
+        }
 
     }
-
     public static Employee getEmployee(){
         if(INSTANCE==null){
             synchronized (Employee.class) {
@@ -17,4 +21,11 @@ public class Employee {
         return INSTANCE;
 
     }
+
+    /*Way to break singleton design pattern
+    * 1) Reflection API.
+    * 2) Serialization and deserialization.
+    *
+    *
+    * */
 }
--- src/singletondesign/main.java
+++ src/singletondesign/main.java
@@ -1,11 +1,21 @@
 package singletondesign;
 
+import java.lang.reflect.Constructor;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Employee employee1 = Employee.getEmployee();
         System.out.println(employee1.hashCode());// hashcode is coming same
         Employee employee2 = Employee.getEmployee();
         System.out.println(employee2.hashCode());//hashcode is coming same
 
+            //1breaking singleton design pattern using refelection api.
+
+            Constructor<Employee> constructor= Employee.class.getDeclaredConstructor();
+            constructor.setAccessible(true); // changing the private constructor to public, changing the Accessiblity.
+            Employee employee3 = constructor.newInstance();
+            System.out.println("employee3" + employee3.hashCode());
+
+            // serialization and deserialization to break the singleton design pattern.
     }
 }
